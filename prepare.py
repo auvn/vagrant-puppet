99258863a25ef922be82c2c8a4cb33b7352709f1
root <root@ip-172-31-37-0.us-west-2.compute.internal>
2015-04-01 08:33:50 -0400

    something new

--- prepare.py
+++ prepare.py
@@ -2,10 +2,15 @@ from string import Template
 
 def config():
     config = {
+        'vm_mem' : '128',
+        'vm_box_name' : 'puppetlabs/centos-6.6-32-puppet',
+        'arch' : 'x86',
         'master_name': 'vm-cluster-node1',
         'slave1_name': 'vm-cluster-node2',
+        'slave2_name': 'vm-cluster-node3',
         'master_ip': '10.211.55.201',
         'slave1_ip': '10.211.55.202',
+        'slave2_ip': '10.211.55.203',
     }
     return config
 
