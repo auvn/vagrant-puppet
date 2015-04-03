45ef0d1db5d38e005fa7ba329de9c39670cb407c
root <root@marshkov-test.iba>
2015-04-03 21:26:53 +0300

    deployer.py uploads the app to target host

--- vagrant/deployer.py
+++ vagrant/deployer.py
@@ -71,41 +71,53 @@ def get_vm_name(connection):
 def get_key_filename(connection):
     return "%s" % connection['IdentityFile']
 
+def test_url_response(url):
+    pass
+
 ############# IMPLEMENTATION ##############
 
-def publish_app(args):
-
-    task_args = args['args']
+def publish_app_task(task_args):
     jar_file = task_args['jar']
 
     if not os.path.isfile(jar_file):
         abort("'%s' doesn't exist." % jar_file)
 
-    connections = args['vm_connections']
-    vm_name = env.host_string
-    connection = connections[vm_name]
-
-    host = get_host(connection)
-    key_filename = get_key_filename(connection)
+    jar_file_name = os.path.basename(jar_file)
+    if not jar_file_name.endswith('.jar'):
+        abort("Looks like '%s' is not a jar file." % jar_file_name)
 
     app_directory = config()['app_directory']
     app_directory = '~/'
 
-    with settings(host_string=host, key_filename=key_filename):
-        run("ls -l %s/%s" % (app_directory, jar_file)
+    target_file = "%s/%s" % (app_directory, 'app.jar')
+
+    put(jar_file, target_file)
+    run('nohup java -jar %s > out.log &' % (target_file), pty=False)
 
 ############# IMPLEMENTATION ###############
 
-def do_fabric(function, args):
-    with settings(parallel=False, warn_only=True):
-        execute(function, hosts = args['hosts'], args = {'vm_connections' : get_vm_connections(), 'args' : args})
+def prepare_connection(function, connections, args):
+    vm_name = env.host_string
+    connection = connections[vm_name]
+    host = get_host(connection)
+    key_filename = get_key_filename(connection)
+    with settings(key_filename=key_filename, warn_only=True):
+        execute(function, host=host, task_args = args)
 
+def do_fabric(function, args):
+    with settings(parallel=False, warn_only=True, hosts = args['hosts']):
+        execute(prepare_connection, function=function, connections = get_vm_connections(), args=args)
+
+
+######## ARGUMENT COMMANDS ##########
 def list_hosts(args):
     vms = get_vms()
     print "Available hosts: ", vms
 
 def install_app(args):
-    do_fabric(publish_app, args)
+    do_fabric(publish_app_task, args)
+
+######## ARGUMENT COMMANDS ##########
 
 
 if __name__ == '__main__':
@@ -117,9 +129,8 @@ if __name__ == '__main__':
     list_hosts_action.set_defaults(func=list_hosts)
 
     install_action = actions.add_parser('install', help='Install a jar to specified host')
-    install_action.add_argument('--jar', required=True)
+    install_action.add_argument('jar')
     install_action.set_defaults(func=install_app)
-
 
     args = vars(arg_parser.parse_args())
     
